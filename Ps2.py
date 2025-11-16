dc239c6f873b87585d76ffd054f4f4ae153fb882
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-11-16 15:54:10 +0530

    Practice sheet 2

--- Ps2.py
+++ Ps2.py
@@ -1,1 +1,15 @@
 print("Practice Sheet 2:")
+
+# W a p to input user's first name & print its length
+str = input("User's First name: ")
+print(str)
+print("Length of the String is: ",len(str))
+
+#W a p to find the occurence of '$' in String.
+
+#ye humne kiya tha 
+print(str.find("$"))
+
+#ye sahi solution hai 
+print(str.count("$"))
+
