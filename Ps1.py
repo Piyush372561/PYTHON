69af058460cf4ee877c5fc4ad2d4cfff8fd1187e
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-11-16 14:49:15 +0530

    Practice Question

---
+++ Ps1.py
@@ -0,0 +1,11 @@
+#PRACTICE SHEET 1 OF PYTHON TUTORIAL
+#w a p to input 2 int numbers, a and b.
+#Print True if a greater Than or equal to b. if not print false
+Num1 = int(input("Num1: "))
+Num2 = int(input("Num2: "))
+if(Num1>=Num2):
+    print("Num1 is greater than Num2")
+else:
+    print("Num2 is greater than Num2")
+
+#W a p 
