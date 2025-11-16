d3d572774de8c0dc5821eac507f3f93e4003425b
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-11-16 14:32:02 +0530

    Adding type conversion

--- TypeConversion.py
+++ TypeConversion.py
@@ -1,1 +1,7 @@
 print("Type Conversions in Python")
+#Type casting
+a,b = "4",7
+#c = int(a)
+d = str(b)
+print(a+d)
+#print(b+c) 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+#Type casting
+a,b = "4",7
+#c = int(a)
+d = str(b)
+print(a+d)
