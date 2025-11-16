b4a149d6da4a6cd6bec290433a76d8656d353703
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-11-16 14:12:55 +0530

    Logical Operators

--- Operators.py
+++ Operators.py
@@ -1,1 +1,8 @@
-print("This File Includes All Operators Related Programs")
+#print("This File includes all Operators related programs")
+
+#Logical operators 
+a = 50
+b = 20
+print(not (a > b))
+print(a>b)
+print((a>b) and (a != b))
