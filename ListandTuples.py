d5914b9662a839522e3b322feff42708d2713e28
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-11-16 18:24:35 +0530

    List

---
+++ ListandTuples.py
@@ -0,0 +1,13 @@
+#List
+
+# 1.
+student = [99.5,49.5,98.6,35.9,39.05]
+print(student)
+print(type(student))
+
+# 2.We can also create a list of different types of elements
+students = ["Arav", 41 , 21.9, True, None]
+print(students) 
+print(type(students))
+students[0] = 'Piyush'
+print(students) 
