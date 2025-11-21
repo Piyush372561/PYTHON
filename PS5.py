22668cb40ae7a8ffd3672ba813750b85d3646669
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-11-21 15:38:52 +0530

    Added new solutions

--- PS5.py
+++ PS5.py
@@ -1,7 +1,7 @@
 print("Practice set 5.")
 
 #Question 1
-for num in range(1,101):
+"""for num in range(1,101):
     print(num)
 
 #Question 2
@@ -9,12 +9,30 @@ for num in range(100,0, -1):
     print(num)
 
 #Question 3
-""" n = int(input("number: "))
-    i = 1
-    while i <= 10 :
-       print(n*i)
-       i += 1  """
-n = int(input("Enter the number: "))
-
+n1 = int(input("Enter the number: "))
 for el in range(1,11):
-     print(n*el)
+     print(n1*el) """
+
+#q
+n = 5
+sum = 0
+i = 1
+while i <= n:
+    sum += i
+    i += 1
+
+print(sum)
+
+"""for i in range(1,n+1):
+    sum += i
+    print(sum) """
+
+#Q 
+n = 5
+fact = 1
+i = 1
+while i <= n:
+    fact *= i
+    i += 1
+
+print("factorial =", fact)
