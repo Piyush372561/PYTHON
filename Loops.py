e7dd63ef0edc2134e73967611ad7ca6fc96f788c
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-11-21 13:18:34 +0530

    Loops

--- Loops.py
+++ Loops.py
@@ -1,6 +1,6 @@
 print("Loops in Python.")
-# while loop 
 
+# while loop 
 # Question --> 1
 """i = 1
 while i <= 100 :
@@ -19,9 +19,7 @@ print("Loop Ended") """
 
 #Question --> 3 Multiplication table of a number n
 n = int(input("number: "))
-table = 0
 i = 1
 while i <= 10 :
-     table += n*i
-     print(table)
+     print(n*i)
      i += 1
