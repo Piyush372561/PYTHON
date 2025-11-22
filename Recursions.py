302361a7e7f33458c1a068e24d726306d4c23dd4
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-11-22 12:46:14 +0530

    Adding new recursion file

--- Recursions.py
+++ Recursions.py
@@ -1,2 +1,21 @@
+print("Recursion.....")
 
+#print the values = 5, 4, 3, 2, 1
+def show(n):
+    if(n == 0):
+        return
+    print(n)
+    show(n-1)
+
+show(5)
+
+#factorial using Recursion
+def factorial(n):
+      if(n == 0 or n == 1): #correct use of or 
+           return 1
+      else:
+           return factorial(n-1) * n
+      
+fact = factorial(1)
+print("Factorial:",fact)
 
