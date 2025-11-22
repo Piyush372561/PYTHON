98569a40ad06ef6078fb878b956af3deb3a67e5f
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-11-22 18:01:10 +0530

    new File

--- fileio.py
+++ fileio.py
@@ -1,15 +1,7 @@
 print("I/O file.....")
 
-f = open("demo.txt","r")#Opening a file
-data = f.read()#reading number of characters
-f2 = open("sample.txt","w")
-f2.write("This is a new line.")
+f = open("sample.txt","r")
+data = (f.read())
 print(data)
-print(type(data))
 
-
-#data = f.read(5)#reading number of characters
-line1 = f.readline()
-print(line1)
-
-f.close()#Closing a file
+f.close()
