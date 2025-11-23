6d3e0c63d85fb5ba78b8a803aae5fd47fe1524b0
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-11-23 15:34:43 +0530

    Add new Files

--- fileio.py
+++ fileio.py
@@ -1,7 +1,15 @@
 print("I/O file.....")
 
-f = open("sample.txt","r")
+"""f = open("sample.txt","r")
 data = (f.read())
 print(data)
 
-f.close()
+f.close() """
+
+with open("sample.txt","r") as f:
+    data = f.read()
+    print(data)
+
+#closed file pe operation perform nhi kr rha 
+with open("sample.txt","w") as f: #but yaha we opened the same file again ?
+    f.write("new data")    
